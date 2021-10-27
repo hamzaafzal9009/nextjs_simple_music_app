5872fbd9e349b1ef8190e98de6a3c90a321df6d2
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2021-10-28 03:29:20 +0500

    Player Details added

--- components/Player.js
+++ components/Player.js
@@ -1,13 +1,16 @@
 import React from "react";
 import style from "../styles/player.module.css";
+import PlayerDetail from "./PlayerDetail";
 
 const Player = (props) => {
   return (
     <div className={style.cplayer}>
       <audio></audio>
       <h4>Playing now</h4>
+      <PlayerDetail song={props.song} />
       <p>
-      <p><strong>Next up: </strong>{props.nextSong.title} by {props.nextSong.artist}</p>
+        <strong>Next up: </strong>
+        {props.nextSong.title} by {props.nextSong.artist}
       </p>
     </div>
   );
