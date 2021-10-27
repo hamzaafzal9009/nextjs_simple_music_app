be690b9267bc6e4cbc5614aad20a737d9b2af7ba
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2021-10-28 03:20:43 +0500

    first commit

---
+++ pages/library.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from 'next/head'
+const library = () => {
+ return (
+   <div>
+      <head>
+       <title>Library</title>
+       <meta name="keywords" content="music, streaming, entertainment"></meta>
+     </head>
+     <h1>Music LIbrary here:</h1>
+   </div>
+);
+};
+export default library;
