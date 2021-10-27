ee3facd4c1803ac8b33f05b36884edc0295993f4
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2021-10-28 03:32:19 +0500

    Player Controls added

--- components/Player.js
+++ components/Player.js
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "../styles/player.module.css";
 import PlayerDetail from "./PlayerDetail";
+import PlayerControls from "./PlayerControls";
 
 const Player = (props) => {
   return (
@@ -8,6 +9,7 @@ const Player = (props) => {
       <audio></audio>
       <h4>Playing now</h4>
       <PlayerDetail song={props.song} />
+      <PlayerControls song={props.song} />
       <p>
         <strong>Next up: </strong>
         {props.nextSong.title} by {props.nextSong.artist}
